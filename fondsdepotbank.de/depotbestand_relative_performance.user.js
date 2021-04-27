60ee01ecfaf8158c789f1c7d87a483e66d1dd88e
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2021-04-27 14:35:33 +0200

    Adjust script after page HTML was changed

--- fondsdepotbank.de/depotbestand_relative_performance.user.js
+++ fondsdepotbank.de/depotbestand_relative_performance.user.js
@@ -1,30 +1,37 @@
 // ==UserScript==
 // @name     relative Fondsperformance Fondsdepotbank
-// @version  3
+// @version  4
 // @grant    none
 // @match    https://finanzportal.fondsdepotbank.de/fdb-*/abaxx-?$part=Home.content.Welcome.Dashboard.4&$event=gotoLink
 // ==/UserScript==
 const getAmount = node => parseFloat(node.innerText.replace('.', '').replace(/,(\d+).*$/, '.$1'));
 
-const createListElementForPercentage = percentage => {
-    const li = document.createElement('li');
-    li.innerHTML = `${percentage.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} %`;
-    li.style.color = percentage < 0 ? 'red' : 'green';
+const createListElementForPercentage = (elementType, percentage) => {
+    const newElement = document.createElement(elementType);
+    newElement.innerHTML = percentage.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    }) + ' %';
+    newElement.style.color = percentage < 0 ? 'red' : 'green';
+    newElement.classList.add('text-right', 'abx-primary');
 
-    return li;
+    return newElement;
 };
 
 const addRelativePerformance = () => {
-    const [saldoNode, gewinnNode] = document.querySelectorAll('.abx-portlet-bnk-portfolioselection-detail div.abx-right > ol > li:first-child');
+    const [saldoNode, gewinnNode] = document.querySelectorAll(
+        '.abx-portlet-bnk-portfolioselection-detail div.abx-panel > dl > dd:nth-child(2)'
+    );
 
     if (saldoNode && gewinnNode) {
         const saldo = getAmount(saldoNode);
         const gewinn = getAmount(gewinnNode);
         const prozente = (gewinn * 100) / (saldo - gewinn);
 
-        const list = document.querySelector('.abx-portlet-bnk-portfolioselection-detail div.abx-left div.abx-right > ol');
-        const li = createListElementForPercentage(prozente);
-        list.append(li);
+        const list = document.querySelector('.abx-portlet-bnk-portfolioselection-detail div.abx-panel > dl');
+        const dd = createListElementForPercentage('dd', prozente);
+        list.append(document.createElement('dt'));
+        list.append(dd);
     } else {
         console.warn('Could not parse the page');
     }
@@ -35,7 +42,7 @@ const addRelativePerformance = () => {
         const gewinn = getAmount(node.childNodes[2]);
         const prozente = (gewinn * 100) / einstandswert;
 
-        const li = createListElementForPercentage(prozente);
+        const li = createListElementForPercentage('li', prozente);
         node.append(li);
     });
 };
