37245c7fa64a5fd25e278a6a13d10927f46d63b7
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2021-05-17 17:57:46 +0200

    Add script for perspective-daily start page

---
+++ perspective-daily.de/README.md
@@ -0,0 +1,8 @@
+# Scripts for perspective-daily.de
+
+## concise_startpage.user.js
+
+Per default, main articles are shown one per row and so-called "dailies" two per row and smaller row height.
+
+This script changes all occurrences of CSS class "longread" to "shortread". Then four article cards can be shown in the
+place of just one before.
---
+++ perspective-daily.de/concise_startpage.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name     Perspective Daily
+// @version  1
+// @grant    none
+// @include  https://perspective-daily.de/article/feed/#/newest*
+// @include  https://perspective-daily.de/member/
+// ==/UserScript==
+
+const changeClass = (node) => {
+    node.className = node.className.replace('longread', 'shortread');
+};
+
+const changeClassInitial = () => {
+    const longreads = document.querySelectorAll('.longread');
+
+    longreads.forEach(node => changeClass(node));
+};
+
+const addObserver = () => {
+    section = document.querySelector('.newest');
+
+    var mutationObserver = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            mutation.addedNodes.forEach(node => changeClass(node));
+        });
+    });
+
+    mutationObserver.observe(section, { childList: true });
+};
+
+document.onreadystatechange = () => {
+    console.log('Trigger for readyState ' + document.readyState);
+    if (document.readyState === 'complete') {
+        changeClassInitial();
+        addObserver();
+    }
+};
